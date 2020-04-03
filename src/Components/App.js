b458d36a3a67d08f3f10627f22ac4d1451e3319f
tuananhkn-2028 <nguyen.tuan.anhk@sun-asterisk.com>
2020-04-03 14:53:08 +0700

    add shopping cart modal

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,48 +3,60 @@ import '../css/App.css';
 import '../css/Item.css';
 import Title from './Title';
 import Searchbar from './Searchbar';
-import Item from './Item'
-import Shoppingcart from './Shoppingcart'
-
+import Item from './Item';
+import Shoppingcart from './Shoppingcart';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {shoppingList: []};
+    this.state = {
+      shoppingList: [],
+      itemList: [
+        {name: 'Shirt 1', price: '100$'},
+        {name: 'Shirt 2', price: '110$'},
+        {name: 'Shirt 3', price: '120$'},
+        {name: 'Pant 1', price: '20$'},
+        {name: 'Pant 2', price: '210$'},
+        {name: 'Pant 3', price: '220$'},
+        {name: 'Jacket 1', price: '300$'},
+        {name: 'Jacket 2', price: '310$'},
+        {name: 'Jacket 3', price: '320$'},
+      ],
+      modalClass: 'modal-hide',
+      showContent: true,
+      count: true,
+    };
     this.addItemToList = this.addItemToList.bind(this);
+    this.handleCartModalClick = this.handleCartModalClick.bind(this);
   }
 
   addItemToList(item) {
     this.setState({shoppingList: [...this.state.shoppingList, item]});
-    
   }
+
+  handleCartModalClick() {
+    this.setState({
+      modalClass: this.state.modalClass === 'modal-hide' ? 'modal-show': 'modal-hide',
+    })
+  }
+
+  
   render() {
+    const { shoppingList, itemList, modalClass, showContent } = this.state;
     return (
       <div className="App">
         <Title />
-        <Searchbar />
-        <Shoppingcart shoppingList={this.state.shoppingList}/>
-        <div className="content">
-        <p>Item List</p>
-          <div className="item-list">
-            <div className="item-row-container">
-              <Item name="Shirt 1" price="100$" addItemToList={this.addItemToList}/>
-              <Item name="Shirt 2" price="110$" addItemToList={this.addItemToList}/>
-              <Item name="Shirt 3" price="120$" addItemToList={this.addItemToList}/>
-            </div>
-            <div className="item-row-container">
-              <Item name="Pant 1" price="200$" addItemToList={this.addItemToList}/>
-              <Item name="Pant 2" price="210$" addItemToList={this.addItemToList}/>
-              <Item name="Pant 3" price="220$" addItemToList={this.addItemToList}/>
-            </div>
-            <div className="item-row-container">
-              <Item name="Coat 1" price="300$" addItemToList={this.addItemToList}/>
-              <Item name="Coat 2" price="310$" addItemToList={this.addItemToList}/>
-              <Item name="Coat 3" price="320$" addItemToList={this.addItemToList}/>
-            </div>
-
-          </div>
-        </div>
+        <Searchbar /> <br />
+        <Button variant="info" className={'show-cart-button'}
+          onClick={this.handleCartModalClick}
+        >View Cart</Button>
+        <Shoppingcart shoppingList={shoppingList} 
+          handleCartModalClick={this.handleCartModalClick}
+          modalClass={modalClass}
+        />
+        {showContent && <Content itemList={itemList} addItemToList={this.addItemToList} />}
         <div className="footer">
           Footer for Clothing.Inc
         </div>
@@ -53,4 +65,20 @@ class App extends React.Component {
   }
 }
 
+const Content = (props) => {
+  const { itemList, addItemToList } = props;
+  return (
+    <div className="content">
+      <p>Item List</p>
+      <div className="item-list">
+        <div className="item-row-container">
+            {itemList.map((item, index) => 
+              <Item key={index.toString()} name={item.name} price={item.price} addItemToList={addItemToList} />
+            )}
+        
+        </div>
+      </div>
+    </div>
+  )
+}
 export default App;
--- src/Components/Shoppingcart.js
+++ src/Components/Shoppingcart.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import '../css/Shoppingcart.css'
-
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
 class Shoppingcart extends React.Component {
-  
 
-  createItemList = () => {
+  createItemList() {
     let ul = [];
     this.props.shoppingList.map((item, index) => {
       ul.push(
         <li key={index.toString()}>{item.name}: {item.price}</li>
       );
     })
-      
-    
-    console.log(ul);
     return ul;
   }
 
-
   render() {
+    const { modalClass } = this.props;
     return (
-      <div className="shopping-cart-modal">
+      <div className={`shopping-cart-modal ${modalClass}`}>
         <p>Your Shopping Cart</p>
         <ul>{this.createItemList()}</ul>
       </div>
